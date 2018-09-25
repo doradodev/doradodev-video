155cc3af9bd101d890c8abe478ccddce887889ae
Christian Dorado <christiandaviddorado@MacBook-Pro-de-Christian-Dorado.local>
2018-09-25 21:56:50 +1000

    initial commit
    React App

---
+++ src/pages/components/related.js
@@ -0,0 +1,13 @@
+import React from 'react';
+// import logo from '../../../images/logo.png';
+import './related.css';
+
+function Related(props) {
+  return (
+    <div className="Related">
+      <h2>Doradodev Video</h2>
+    </div>
+  )
+}
+
+export default Related;
